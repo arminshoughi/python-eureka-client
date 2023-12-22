346e6b84beab73fb8063024206a59da543fad9c4
Armin Shoughi <a.shoughi@hesaba.co>
2023-12-22 18:02:37 +0330

    chore(struc): reviewed netint file

--- py_eureka_client/netint_utils.py
+++ py_eureka_client/netint_utils.py
@@ -1,30 +1,7 @@
-# -*- coding: utf-8 -*-
-
-"""
-Copyright (c) 2018 Keijack Wu
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-"""
-import socket
+import socket, ipaddress
 from typing import Tuple
 from ifaddr import get_adapters
-import ipaddress
+
 from py_eureka_client.logger import get_logger
 
 _logger = get_logger("netint_utils")
@@ -34,7 +11,7 @@ def get_host_by_ip(ip):
     try:
         return socket.gethostbyaddr(ip)[0]
     except:
-        _logger.warn("Error when getting host by ip", exc_info=True)
+        _logger.warning("Error when getting host by ip", exc_info=True)
         return ip
 
 
