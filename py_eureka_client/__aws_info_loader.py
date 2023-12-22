beeb02b1de3f47ee0c6444eefaafc2de2291b35a
Armin Shoughi <a.shoughi@hesaba.co>
2023-12-22 18:05:37 +0330

    chore(struc): reviewed aws info loader

--- py_eureka_client/__aws_info_loader.py
+++ py_eureka_client/__aws_info_loader.py
@@ -1,31 +1,6 @@
-# -*- coding: utf-8 -*-
-
-"""
-Copyright (c) 2018 Keijack Wu
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-"""
-import socket
-import json
-import time
-from py_eureka_client.http_client import http_client
+import socket, json, time
 from py_eureka_client.logger import get_logger
+from py_eureka_client.http_client import http_client
 
 _logger = get_logger("aws_info_loader")
 
